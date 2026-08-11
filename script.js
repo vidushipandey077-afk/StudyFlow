0eaddde314248aba3995369c8707b70bd0c9147b
Vidushi Pandey <vidushipandey077@gmail.com>
2026-08-12 01:00:57 +0530

    New Updates to Daily Tasks

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ const backButton=document.getElementById('backButton')
 const addButton=document.getElementById("addButton")
 const taskList=document.getElementById("taskList")
 const inputTask=document.getElementById("taskInput")
+const pendingCount=document.getElementById("pendingCount")
+const completedCount=document.getElementById("completedCount")
 
 dailyTaskCard.addEventListener('click',dailyTaskCardClick)
 backButton.addEventListener('click',goBack)
 addButton.addEventListener('click',addTask)
+taskList.addEventListener('change',updateTask)
 
-
+function updateTask(){
+    let completedTask=0;
+    let pendingTask=0;
+    const checkboxes=taskList.querySelectorAll("input")
+    checkboxes.forEach((val)=>{
+        if(val.checked){
+            completedTask++;
+        }
+        else{
+            pendingTask++;
+        }
+    })
+    completedCount.textContent=completedTask
+    pendingCount.textContent=pendingTask
+}
 function dailyTaskCardClick(){
     dashboard.style.display="none"
     dailyTasks.style.display="flex"
@@ -23,7 +40,9 @@ function goBack(){
 
 function addTask(){
     const input=inputTask.value;
-
+    if(input.trim()==""){
+        return;
+    }
     const checkbox=document.createElement("input")
     checkbox.type="checkbox"
 
@@ -36,4 +55,5 @@ function addTask(){
     inputList.appendChild(typeTask)
 
     taskList.appendChild(inputList)
+    updateTask()
 }
